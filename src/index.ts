78c3e66d743edf4beaba3df3fca6cbb1caeecd89
Helder S Ribeiro <hsribei.pub@gmail.com>
2017-12-27 11:19:23 -0300

    Draw base svg

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import "./style.css";
+import * as d3 from "d3";
 
 function makeHeatMap(fullData): void {
   const data = fullData.monthlyVariance;
-  console.log(data.length);
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 },
+    totalWidth = 1024,
+    totalHeight = 768,
+    width = totalWidth - margin.left - margin.right,
+    height = totalHeight - margin.top - margin.bottom;
+
+  const svg = d3
+    .select("#d3")
+    .append("svg")
+    .attr("width", totalWidth)
+    .attr("height", totalHeight)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 }
 
 function fetchAndExecuteWithJson(
