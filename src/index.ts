0eb9ba62c6ee46bb766c461ebffe741bd2c0fd42
Helder S Ribeiro <hsribei.pub@gmail.com>
2017-12-27 11:16:57 -0300

    Minimal fetch and print data.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import "./style.css";
 
+function makeHeatMap(fullData): void {
+  const data = fullData.monthlyVariance;
+  console.log(data.length);
+}
+
+function fetchAndExecuteWithJson(
+  url: string,
+  jsonConsumer: (json: object) => void
+): void {
+  fetch(url).then(response => response.json().then(jsonConsumer));
+}
+
 document.addEventListener("DOMContentLoaded", event => {
-  console.log("hay!");
+  const url =
+    "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+  fetchAndExecuteWithJson(url, makeHeatMap);
 });
