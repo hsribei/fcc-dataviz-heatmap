19089466c34b7f2e059280b662162b3dc41fb5a6
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-01-13 08:29:47 -0300

    Color.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import "./style.css";
 import "core-js";
 import * as d3 from "d3";
 
+function heatMapColorforValue(value) {
+  var h = (1.0 - value) * 240;
+  return "hsl(" + h + ", 100%, 50%)";
+}
+
 function makeHeatMap(fullData): void {
   const data = fullData.monthlyVariance;
   const margin = { top: 20, right: 20, bottom: 20, left: 100 },
-    totalWidth = 2048,
-    totalHeight = 480,
+    totalWidth = 1240,
+    totalHeight = 400,
     width = totalWidth - margin.left - margin.right,
     height = totalHeight - margin.top - margin.bottom;
 
@@ -36,6 +41,11 @@ function makeHeatMap(fullData): void {
       }
     }
   }
+
+  // Let there be color
+  const normalizeVariance = d3.scaleLinear().range([0, 1]);
+  normalizeVariance.domain(d3.extent(data, d => d.variance));
+  const color = d => heatMapColorforValue(normalizeVariance(d.variance));
 
   // Get the bands together! (Draw rectangles)
   const xBand = d3.scaleBand().range([0, width]);
@@ -75,8 +85,7 @@ function makeHeatMap(fullData): void {
     .attr("y", d => yBand(monthNames[d.month - 1]))
     .attr("width", xBand.bandwidth())
     .attr("height", yBand.bandwidth())
-    .style("fill", "#000")
-    .style("stroke", "#fff");
+    .style("fill", d => color(d));
 
   // Add the X Axis
   const xTime = d3.scaleTime().range([0, width]);
@@ -111,3 +120,4 @@ document.addEventListener("DOMContentLoaded", event => {
 
 // Bibliography
 // * Let's Make a Grid with D3.js: https://bl.ocks.org/cagrimmett/07f8c8daea00946b9e704e3efcbd5739
+// * Heatmap color for value: https://stackoverflow.com/a/27263918/105132
